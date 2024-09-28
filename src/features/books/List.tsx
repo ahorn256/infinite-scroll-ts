43644d231425448ccc0f8393d2e76160d5d741ff
ahorn256 <anton.horn@futuristen.education>
2024-09-28 14:17:41 +0200

    added react-window library

--- src/features/books/List.tsx
+++ src/features/books/List.tsx
@@ -2,6 +2,9 @@ import { Paper, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/ma
 import { Star, StarBorder } from "@mui/icons-material";
 import { Book } from "./Book";
 import styles from './List.module.scss';
+import React, { CSSProperties, ReactNode } from "react";
+import { FixedSizeList, ListChildComponentProps } from "react-window";
+import AutoSizer from "react-virtualized-auto-sizer";
 
 const tableHead = {
   title: 'Title',
@@ -19,30 +22,58 @@ const books:Book[] = new Array(100).fill(
   }
 ).map((book, id) => ({...book, id: id.toString()}));
 
+type InnerProps = {
+  children: ReactNode,
+  style: CSSProperties,
+}
+const Inner:React.FC<InnerProps> = ({ children, style }) => {
+  return (
+    <Table>
+      <TableHead>
+        <TableRow>
+          {Object.entries(tableHead).map(([key, head]) => (
+            <TableCell key={key}>
+              {head}
+            </TableCell>
+          ))}
+        </TableRow>
+      </TableHead>
+      <TableBody style={{...style, position:'absolute', width:'100%'}}>
+        {children}
+      </TableBody>
+    </Table>
+  );
+}
+
+const Row:React.FC<ListChildComponentProps<Book[]>> = ({ index, data: books, style }) => {
+  const book = books[index];
+
+  return (
+    <TableRow style={style}>
+      <TableCell>{book.title}</TableCell>
+      <TableCell>{book.author}</TableCell>
+      <TableCell>{book.isbn}</TableCell>
+      <TableCell>{Array(5).fill(0).map((item, index) => index < book.rating ? <Star key={index} /> : <StarBorder key={index} />)}</TableCell>
+    </TableRow>
+  );
+}
+
 function List() {
   return (
-    <Paper className={styles.list}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            {Object.entries(tableHead).map(([key, head]) => (
-              <TableCell key={key}>
-                {head}
-              </TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {books.map(book => (
-            <TableRow key={book.id}>
-              <TableCell>{book.title}</TableCell>
-              <TableCell>{book.author}</TableCell>
-              <TableCell>{book.isbn}</TableCell>
-              <TableCell>{Array(5).fill(0).map((item, index) => index < book.rating ? <Star key={index} /> : <StarBorder key={index} />)}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+    <Paper className={styles.list} style={{height:'500px', maxHeight:'calc(100vh - 100px)' }}>
+      <AutoSizer>
+        {({width, height}:{width:number, height:number}) => (
+          <FixedSizeList
+            width={width}
+            height={height}
+            itemSize={62}
+            itemCount={books.length}
+            itemData={books}
+            innerElementType={Inner}>
+            {Row}
+          </FixedSizeList>
+        )}
+      </AutoSizer>
     </Paper>
   );
 }
